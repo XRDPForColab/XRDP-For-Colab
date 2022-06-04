9802616f8b08091978ea7cf36dbb9264d093d651
XRDPForColab <106880527+XRDPForColab@users.noreply.github.com>
2022-06-04 21:20:59 +0300

    Update Script.sh

--- Script.sh
+++ Script.sh
@@ -2,17 +2,15 @@ wget https://xforc.000webhostapp.com/ngrok > /dev/null 2>&1
 clear
 read -p "Paste authtoken here (Copy and Ctrl+V to paste then press Enter): " CRP
 ./ngrok authtoken $CRP 
-./ngrok tcp 3389 &>/dev/null &
+nohup ./ngrok tcp 3389 &>/dev/null &
 echo Updating packages...
 sudo apt update -y > /dev/null 2>&1
-echo "Installing Ubuntu Desktop and XRdp"
+echo "Installing Ubuntu Desktop and xrdp"
 sudo apt install ubuntu-desktop xrdp curl -y > /dev/null 2>&1
 sudo service xrdp start
-echo Please add a password..
+echo Please add a user...
 sudo adduser ubuntu
 sudo usermod -aG sudo ubuntu
-echo Your RDP username:
-echo ubuntu
 echo Your RDP IP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo "Note: Use Right-Click To Copy"
